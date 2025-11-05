4fa46e8750bc83d85e06d826aa74fd8dacfe9524
bitrifttech <matthew@bitrift.tech>
2025-11-05 13:12:46 -0600

    Update formula with correct SHA256 for v0.1.0

--- li.rb
+++ li.rb
@@ -5,7 +5,7 @@ class Li < Formula
   desc "CLI assistant that converts natural language to shell plans using AI"
   homepage "https://github.com/bitrifttech/li"
   url "https://github.com/bitrifttech/li/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "sha256_placeholder"  # Will be updated after creating release
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
   depends_on "rust" => :build
