8cff8960b90f1cad9a6a2016ee4be8a3559decd7
bitrifttech <matthew@bitrift.tech>
2025-11-05 14:53:52 -0600

    Update to v0.1.1

--- li.rb
+++ li.rb
@@ -4,8 +4,8 @@
 class Li < Formula
   desc "CLI assistant that converts natural language to shell plans using AI"
   homepage "https://github.com/bitrifttech/li"
-  url "https://github.com/bitrifttech/li/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "1407d1b7041a6b5362556a2e282a603762dff8d861538ee2c0ec9b574201b5c4"
+  url "https://github.com/bitrifttech/li/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "6b5713df6be6a8c8b7af0e0e50011b2dce606b07f034af86c088297219d1ac40"
   license "MIT"
   version "0.1.1"
 
